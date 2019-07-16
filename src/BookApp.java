08343ebec08fa64e3d9ca70ac8c1ecbb7a09d00d
Leya Coleman <leyacole99@gmail.com>
2019-07-16 10:47:00 -0400

    Leya's menu additions to BookApp

--- src/BookApp.java
+++ src/BookApp.java
@@ -22,6 +22,7 @@ public class BookApp {
 		
 		boolean Valid = true;
 		int userChoice = 0;
+		String input = "";
 		int serialNumb = 0;
 		do {
 			System.out.println("Are you here to\n 1. Get a book?\n 2. Return a book?\n 3. Nahhhh save dem trees");
@@ -30,6 +31,22 @@ public class BookApp {
 			if (userChoice == 1) {
 				System.out.println(" 1. Search by Author\n 2. Search by Title\n 3. Search by Genre");
 				userChoice = scnr.nextInt();
+				scnr.nextLine();
+					if(userChoice == 1) {
+						System.out.println("Enter Author name: ");
+						input = scnr.nextLine();
+						//call to check list
+					}else if (userChoice == 2) {
+						System.out.println("Enter Title name: ");
+						input = scnr.nextLine();
+						//call to check list
+					}else {
+						System.out.println("Enter Genre: ");
+						input = scnr.nextLine();
+						//call to check list
+					}
+						
+					
 			} else if (userChoice == 2) {
 				System.out.println("Enter the serial number please.");
 				serialNumb = scnr.nextInt();
